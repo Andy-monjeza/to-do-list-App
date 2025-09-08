445de6074373b8eb64f9f54971fb81c231ba50ce
Andy-monjeza <wangamonjeza@gmail.com>
2025-09-08 16:05:32 +0300

    input new copy of todolist.js

--- todolist.js
+++ todolist.js
@@ -3,11 +3,15 @@ const andy=new todolist();
 
 andy.updateStats();
 
-
 const menuBtn=document.querySelector('.menu');
 const popup=document.querySelector('.popup');
 const inputField=`
  <div class="input-field">
+  <button class="back-btn">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M15 18l-6-6 6-6"/>
+  </svg>
+</button>
         <input class="todo" placeholder="To-do">
         <span class="date-tag">Due on:</span>
         <input type="date" class="date-picker">
@@ -16,15 +20,11 @@ const inputField=`
 `;
 const optionField=`
 <div class="option-field">
-      <button class="edit">
-                 
-  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#2563eb" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
-    <path d="M12 20h9"/>
-    <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4 12.5-12.5z"/>
+       <button class="back-btn">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M15 18l-6-6 6-6"/>
   </svg>
-
-  Edit task</button>
-
+</button>
  <button class="mark-complete">
   <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#2563eb" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
     <path d="M5 13l4 4L19 7"/>
@@ -42,7 +42,6 @@ menuBtn.addEventListener("click",()=>{
    popup.innerHTML=inputField;
 
 })
-   
 
 popup.addEventListener("click",(e)=>{
   if(e.target.classList.contains('add-task')){
@@ -50,12 +49,13 @@ popup.addEventListener("click",(e)=>{
     const date=document.querySelector('.date-picker');
        andy.addTask(task.value,date.value);  
        andy.updateTasks();
+       andy.updateStats();
        popup.style.display="none";
   }
+  else if(e.target.closest('.back-btn')){
+  popup.style.display="none";
+  }
+  else if(e.target.classList.contains('')){
+ popup.innerHTML=inputField;
+  }
 })
-
-
-
-
-const crntDate=new Date();
-console.log(crntDate);
