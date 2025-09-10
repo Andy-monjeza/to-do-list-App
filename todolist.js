5a72e54854eb5675b8ff40374ac2524d6adab40c
Andy-monjeza <wangamonjeza@gmail.com>
2025-09-10 18:42:25 +0300

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -1,8 +1,6 @@
 import { todolist } from "./UI-updates.js";
 const andy=new todolist();
 
-andy.updateStats();
-
 const menuBtn=document.querySelector('.menu');
 const popup=document.querySelector('.popup');
 const inputField=`
@@ -18,23 +16,6 @@ const inputField=`
         <button class="add-task">Add</button>
     </div>
 `;
-const optionField=`
-<div class="option-field">
-       <button class="back-btn">
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#444" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-    <path d="M15 18l-6-6 6-6"/>
-  </svg>
-</button>
- <button class="mark-complete">
-  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#2563eb" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
-    <path d="M5 13l4 4L19 7"/>
-  </svg>
-          
-     Done</button>
-     
-     
-      <button class="delete">Delete</button>
-`
 menuBtn.addEventListener("click",()=>{
  if(popup.style.display="none"){
         popup.style.display="block";
